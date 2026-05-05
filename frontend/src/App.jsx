db90cad3c93a5ca6b9d0e44882ef2179ee1f54e8
lovelu99 <lovelu@gmail.com>
2026-05-04 23:24:16 -0400

    test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,8 @@ function App() {
   return (
     <div style={{ padding: "20px", fontFamily: "Arial" }}>
       <h1>3-Tier Azure Devops Demo App </h1>
-      <h2>self hosted runner and Gitops!!!</h2>
+      <h2>self hosted runner and Gitops!!</h2>
+
       <p>React frontend + ASP.NET Core API + Azure SQL + Key Vault</p>
 
       {error && <p>{error}</p>}
